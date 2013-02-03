17aaba000d46588655a6314d8d68c9ef15bdb43c
Willy Wonka <akhlaq87@gmail.com>
2013-02-03 12:39:35 +0100

    search similar title under the album name

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 require 'terminal-notifier'
+require 'text'
 
 
 album = `osascript -e'tell application "iTunes"' -e'get album of current track' -e'end tell'`.chop!.to_s
@@ -28,6 +29,47 @@ doc = Nokogiri::HTML(open(url))
    lyrics += "\n\n"
  end
 
+ 
+ if lyrics==""
+   	title = `osascript -e'tell application "iTunes"' -e'get name of current track' -e'end tell'`.chop!.to_s
+   	url = "http://www.paksmile.com/lyrics/#{album}/"
+   	doc = Nokogiri::HTML(open(url))
+
+  	album_titles={}
+  	doc.xpath('//table[@bgcolor="#F2FCFF"]//strong/a').each do |ly|
+    		album_titles["#{ly.to_s.scan(/>([^"]*)<\/a>/)}"]= ly.to_s.scan(/href="([^"]*)"/)
+  	end
+
+
+    best_match=0
+    link_to_best_hit=""
+  	white = Text::WhiteSimilarity.new
+  	album_titles.each do |k,v|
+      if white.similarity(title, k) > best_match
+        link_to_best_hit = v
+      end
+  	end
+    puts link_to_best_hit
+    puts album
+    
+    
+    # dirty solution
+   url = "http://www.paksmile.com/lyrics/#{album}/#{link_to_best_hit.to_s}"
+   puts url
+    doc = Nokogiri::HTML(open(url))
+ 
+     lyrics=""
+     doc.xpath('//td[@bgcolor="F2FCFF"]/p').each do |ly|
+       lyrics += ly
+       lyrics += "\n\n"
+     end
+    #end dirty solution
+
+    puts lyrics
+    
+ end
+ 
+
 
 if lyrics==""
   TerminalNotifier.notify("Album: #{album.gsub('-',' ').capitalize}", :title => 'Fehlgeschlagen', :subtitle => "#{title.gsub('-',' ')}")
@@ -36,5 +78,3 @@ else
   TerminalNotifier.notify("Album: #{album.gsub('-',' ').capitalize}", :title => 'Erfolreich', :subtitle => "#{title.gsub('-',' ')}")
   
 end
-
-#puts lyrics
