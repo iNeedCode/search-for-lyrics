df08f7d4971bab9466a5201670b897998ec5cbde
Willy Wonka <akhlaq87@gmail.com>
2013-02-04 23:39:49 +0100

    added the resources

--- lib/lyric_resource.rb
+++ lib/lyric_resource.rb
@@ -49,23 +49,51 @@ class LyricResource
   
   def search_for_lyrics
     return true if search_under_bollywoodlyrics
+    return true if search_under_paksmile    
     return false
-  end
-  
-  def open_link(url)
-    begin
-      doc = Nokogiri::HTML(open(url))
-    rescue Exception => ex
-      puts "Error: #{ex}"
-      @notification[:message] = "Fehler #{ex}"
-      @notification[:title] = "Fehler"
-      return false
-    end
-    doc
   end
   
   def search_under_bollywoodlyrics
     @notification[:message] = "www.bollywoodlyrics.com"
+    lyrics=""
+    album=@track[:album].downcase
+    album = album.gsub(' ','-') if album.split.size > 1
+    
+    url = "http://www.bollywoodlyrics.com/movie_name/#{album}"
+    doc = open_link(url)
+    return false unless doc
+    
+    #save all album titles
+    album_titles={}
+    doc.xpath('//p[@class="entry-title"]/a').each do |ly|
+    		album_titles["#{ly.to_s.scan(/>([^"]*)<\/a>/)}"]= ly.to_s.scan(/href="([^"]*)"/)
+    end
+ 
+    find_title = find_album_title_on_page(album_titles)
+    
+    unless find_title==""
+      doc = open_link(find_title.to_s)
+        doc.xpath('//div[@class="entry-content"]/pre').each do |line|
+          lyrics += line
+        end
+        
+      # #############################        TODO         ###########################  
+      # Tadap Tadap Example  
+      # unless lyrics.size > 50 
+      #  doc.xpath('//div[@class="entry-content"]/p').each do |line|
+      #    lyrics += "#{line}\n"
+      #  end
+      #end
+    end
+    #lyrics = lyrics.delete(lyrics.to_s.scan(/<p class(.*)/))
+    puts lyrics
+    
+    set_notification(lyrics)
+    return @notification[:found]
+  end
+  
+  def search_under_paksmile
+    @notification[:message] = "www.paksmile.com"
     lyrics=""
     url = "http://www.bollywoodlyrics.com/movie_name/#{@track[:album]}"
     doc = open_link(url)
@@ -126,6 +154,18 @@ class LyricResource
       @notification[:found] = true
       @notification[:title] = "Erfolgreich"
     end
+  end
+  
+  def open_link(url)
+    begin
+      doc = Nokogiri::HTML(open(url))
+    rescue Exception => ex
+      puts "Error: #{ex}"
+      @notification[:message] = "Fehler #{ex}"
+      @notification[:title] = "Fehler"
+      return false
+    end
+    doc
   end
   
 end
